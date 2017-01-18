5a23cfba07224922723a39728bf81b87d091fcad
David King <david@oodavid.com>
2017-01-18 00:41:20 +0000

    Ledger > Minor

--- app/scripts/ledgerService.js
+++ app/scripts/ledgerService.js
@@ -11,12 +11,13 @@
     .service('ledgerService', ['gameLoop', function(gameLoop){
         var _this = this;
         this.balance = 0;
+        var date = null; // Current month
+        this.diff = 0;   // Difference for this month
         this.ledger = [];
         this.track = function(type, name, value){
             if(value){
                 var IN  = value > 0 ?  value : null;
                 var OUT = value < 0 ? -value : null;
-                this.balance += value;
                 var props = {
                     date: gameLoop.date,
                     type: type,
@@ -25,6 +26,14 @@
                     out: OUT,
                     balance: this.balance,
                 }
+                // Add to this months diff
+                if(gameLoop.date != date){
+                    date = gameLoop.date
+                    this.diff = 0;
+                }
+                this.diff += value;
+                // Add to the balance
+                this.balance += value;
                 // Add to the ledger
                 _this.ledger.push(props);
             }
